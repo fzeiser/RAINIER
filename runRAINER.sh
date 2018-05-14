ad052fe1af9106fba58c27a87ba3b0602f8b327b
fzeiser <fzeiser@physnet.uni-hamburg.de>
2018-05-14 21:54:59 +0200

    Correct spelling

--- runRAINER.sh
+++ runRAINER.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/expect
 
-# to set RAINER_PATH, execute the following in terminal, or save in .bashrc/profile
-# export RAINER_PATH=/path/to/RAINIER
+# to set RAINIER_PATH, execute the following in terminal, or save in .bashrc/profile
+# export RAINIER_PATH=/path/to/RAINIER
 
-if { [info exists RAINER_PATH] } {
-	send "\nRAINER_PATH is unset!\n"
+if { [info exists RAINIER_PATH] } {
+	send "\nRAINIER_PATH is unset!\n"
     } else {
 
 	   	# if settings.h exisits in the RAINIER folder, this will be read by RAINIER instead.
-		if {[file exists $env(RAINER_PATH)/settings.h]} {
+		if {[file exists $env(RAINIER_PATH)/settings.h]} {
    		send "Cannot run -- settings.h exists also in RAINIER folder\n"
 		} else {
 		spawn root -l 
@@ -17,9 +17,9 @@ if { [info exists RAINER_PATH] } {
 
 		send "dir = \".include \" + gSystem->GetWorkingDirectory()\r"
 		send "gROOT->ProcessLine(dir.c_str())\r"
-		expect -timeout -1 "root" { send ".x $env(RAINER_PATH)/RAINIER.C++\r"}
-		# send "gROOT->ProcessLine(\".L $env(RAINER_PATH)/Analyze.C++\")\r"
-		send ".L $env(RAINER_PATH)/Analyze.C++\r"
+		expect -timeout -1 "root" { send ".x $env(RAINIER_PATH)/RAINIER.C++\r"}
+		# send "gROOT->ProcessLine(\".L $env(RAINIER_PATH)/Analyze.C++\")\r"
+		send ".L $env(RAINIER_PATH)/Analyze.C++\r"
 		send "RetrievePars()\r"
 		interact
 		}
